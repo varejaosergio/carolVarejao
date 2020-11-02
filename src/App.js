5839600ec43cc2d9a04d5fb94c4241a9d05b4506
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2020-11-02 15:53:57 -0800

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Form from './components/Form/Form';
 import PageDefault from './components/PageDefault';
+import {Container} from '@material-ui/core';
 
 class App extends Component {
   render(){
     return (
       <PageDefault>
-  
+        <Container>
+          <Form/>
+        </Container>        
       </PageDefault>
     );
   }
