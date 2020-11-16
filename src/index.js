cacb8f9ca5eceaca6af2e9a39c168b3d6d1e3959
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2020-11-16 10:32:07 -0800

    mudanças para single page aplication

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/* SSV alteração para utilizar o modelo single page, 
+todos os imports abaixo e implementação são as mudanças para SPA
+ import App from './App';*/
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  /* SSV implementação SPA, sai esse config <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*/
+  <BrowserRouter>
+     <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />       
+      <Route component={() => (<div>Página 404</div>)} />
+     </Switch>
+  </BrowserRouter>,  
   document.getElementById('root')
 );
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,35 +2,39 @@ import React from "react";
 import PageDefault from "../../components/PageDefault";
 import dadosIniciais from "../../data/dados_iniciais.json";
 import BannerMain from "../../components/BannerMain";
-import { Container } from "@material-ui/core";
 import About from "../../components/About";
 import Carousel from "../../components/Carousel";
+
+import { Container, Typography } from "@material-ui/core";
 import Form from "../../components/Form/Form";
+import Mentoria from "../../components/Mentoria";
 
 function Home() {
-    /*const [dadosIniciais, setDadosIniciais] = useStata([]);
+  return (
+    <PageDefault>
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription="Mentora Carol Varejão 'Você Marca!'"
+      />
+      <About />
+      <Carousel category={dadosIniciais.categorias[0]} />
 
-    useEffect(() => {
-        categoriasRepository.getAllWithVideos()
-        const URL = '';
+      <Mentoria />
 
-    }, []);*/
-    return (
-      <PageDefault>
-        <BannerMain
-          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-          url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription="Mentora Carol Varejão 'Você Marca!'"
-        />
-        <About />
-        <Carousel category={dadosIniciais.categorias[0]} />
-
-        <Container component="article" maxWidth="sm">
-          <Form onSubmit={onSubmitForm} />
-        </Container>
-      </PageDefault>
-    );
-  
+      <Container component="article" maxWidth="sm">
+        <Typography
+          variant="h3"
+          component="h1"
+          align="center"
+          color="textPrimary"
+        >
+          MENTORIA VOCÊ MARCA!
+        </Typography>
+        <Form onSubmit={onSubmitForm} />
+      </Container>
+    </PageDefault>
+  );
 }
 
 function onSubmitForm(data) {
